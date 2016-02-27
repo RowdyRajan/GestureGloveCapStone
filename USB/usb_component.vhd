337e7e3e4743c7bbd47bc298ad29fcd9b4e7df29
Eric Smith <egsmith@ualberta.ca>
2016-02-27 15:35:42 -0700

    update to architecture, not finsihed yet

--- USB/usb_component.vhd
+++ USB/usb_component.vhd
@@ -9,15 +9,15 @@ entity usb is
 		clk : in std_logic;
 		reset_n : in std_logic;
 
-		-- OTG Conduit interface to DE2 (Export)
-		OTG_DATA	: inout std_logic_vector(15 downto 0);
-		OTG_ADDR	: out std_logic_vector(1 downto 0);
-		OTG_WR_N	: out std_logic := '1';
-		OTG_RD_N	: out std_logic := '1';
-		OTG_RST_N	: out std_logic := '1';
-		OTG_CS_N	: out std_logic := '1';
-		OTG_INT0	: in std_logic; 	-- Irq 0 DC
-		OTG_INT1	: in std_logic; 	-- Irq 1 HC
+		-- USB Conduit interface to DE2 (Export)
+		USB_DATA	: inout std_logic_vector(15 downto 0);
+		USB_ADDR	: out std_logic_vector(1 downto 0);
+		USB_WR_N	: out std_logic := '1';
+		USB_RD_N	: out std_logic := '1';
+		USB_RST_N	: out std_logic := '1';
+		USB_CS_N	: out std_logic := '1';
+		USB_INT0	: in std_logic; 	-- Irq 0 DC
+		USB_INT1	: in std_logic; 	-- Irq 1 HC
 	
 		-- Avalon Memory-Mapped-Slave interface Device Controller (DC)
 		avs_dc_address		: in std_logic_vector(1 downto 0);
@@ -25,7 +25,7 @@ entity usb is
 		avs_dc_write_n		: in std_logic;
 		avs_dc_read_n		: in std_logic;
 		avs_dc_readdata		: out std_logic_vector(15 downto 0);
-		avs_dc_irq		: out std_logic;
+		avs_dc_irq			: out std_logic;
 	
 		-- Avalon Memory-Mapped-Slave interface Host Controller (HC)
 		-- Probably will not use this interface. 
@@ -34,18 +34,29 @@ entity usb is
 		avs_hc_write_n		: in std_logic
 		avs_hc_read_n		: in std_logic;
 		avs_hc_readdata		: out std_logic_vector(15 downto 0);
-		avs_hc_irq		: out std_logic;
+		avs_hc_irq			: out std_logic;
 	
 	);
 end usb;
 	
 architecture connections of usb is'
 
+	signal 
 
 	begin
 	-- Send interrupt from DE2 connection to proper controller
-	avs_dc_irq <= OTG_INT0;
-	avs_hc_irq <= OTG_INT1;
+	avs_dc_irq <= USB_INT0;
+	avs_hc_irq <= USB_INT1;
+	
+	--Two cases possible, using the host controller or the device controller. The github project combines them with when else style assignments
+	
+	USB_DATA <= avs_dc_writedata when avs_dc_write_n = '0' else (others => 'Z'); -- Only does device controller
+	avs_dc_readdata <= USB_DATA when avs_dc_read_n = '0' else (others => 'Z');
+	avs_hc_readdata <= USB_DATA when avs_hc_read_n = '0' else (others => 'Z');
+	
+	--USB_ADDR <= avs_dc_address; -- Currently this passes both bits from DC to DE2. 
+	
+	-- If chip_select_n == 1
 
 
 
